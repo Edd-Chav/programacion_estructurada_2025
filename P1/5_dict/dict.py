8a78b7f68b261647a7b6d4bb407244764b89930c
Edd-Chav <eduardo_3141240140@utd.edu.mx>
2025-07-21 20:46:39 -0600

    Add files via upload

---
+++ P1/5_dict/dict.py
@@ -0,0 +1,23 @@
+'''Es un tipo de datos que se utiliza para almacenar datos de diferentes tipo
+de deatos pero en lugar de tener como las listas numericos tiene alfanumericos.
+Es decir es algo parecido como los objetos
+
+Tambien se conoce como un arreglo asosiativo u Objeto.json
+
+El diccionario es una coleccion y modificable. No hay miembros duplicados'''
+
+import os
+os.system("Clear")
+
+#paises={"Mexico","Brasil","España","Canada"}
+
+
+#dict o objeto
+paises_mexico={"nombre","Mexico","Capital","CDMEX","Poblacion,","1200000000","Idioma","Español" :True}
+
+paises_brasil={"nombre","Brasil","Capital","Brasilia","Poblacion,","3000000000","Idioma","Portuges" :True}
+
+paises_brasil={"nombre","Canadal","Capital","Ottowa","Poblacion,","500000000","Idioma","Prances" :False}
+
+
+alumnos1={"Nombre","Daniel","Apellido","Hernandez","Apellido 2","Gonzalez","Carrera","TI","Area","Software Multiplataforma"."Modalidad","Bilingue","Matricula"."1234567"}
